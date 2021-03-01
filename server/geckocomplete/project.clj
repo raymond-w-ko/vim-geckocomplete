c8cb75d829ba3f1c9399b8973da54b3ab708a767
Raymond W. Ko <raymond.w.ko@gmail.com>
2021-03-01 16:15:31 -0500

    make java auto-compile using lein-virgil

--- server/geckocomplete/project.clj
+++ server/geckocomplete/project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot geckocomplete.core
   :target-path "target/%s"
-  :plugins [[lein-tools-deps "0.4.5"]]
+  :plugins [[lein-tools-deps "0.4.5"]
+            [lein-virgil "0.1.9"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :java-source-paths ["src/java"]
   :lein-tools-deps/config {:config-files [:project]}

0d9f73eb54ccc0f5b324ca1b92ee5dbb3fef29f6
Raymond W. Ko <raymond.w.ko@gmail.com>
2021-03-02 01:24:50 -0500

    add JVM args required for profiling

--- server/geckocomplete/project.clj
+++ server/geckocomplete/project.clj
@@ -3,6 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :jvm-opts ["-Djdk.attach.allowAttachSelf"
+             "-XX:+UnlockDiagnosticVMOptions"
+             "-XX:+DebugNonSafepoints"
+             "-XX:-OmitStackTraceInFastThrow"]
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot geckocomplete.core
   :target-path "target/%s"
